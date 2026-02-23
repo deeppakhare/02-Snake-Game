df21eac6a63691cec8c7a4c6b51a53a6e53b1312
justforit4102 <deeppakhare19@gmail.com>
2026-02-23 16:32:16 +0530

    apply snake direction

--- script.js
+++ script.js
@@ -27,6 +27,36 @@ for (let row = 0; row < rows; row++) {
     }
 }
 
+function render() {
+    snake.forEach(segment => {
+        blocks[`${segment.x}-${segment.y}`].classList.add("fill");
+    })
+}
+
+setInterval(() => {
+
+    let head = null;
+
+    if (direction === "left") {
+        head = { x: snake[0].x, y: snake[0].y - 1 }
+    } else if (direction === "right") {
+        head = { x: snake[0].x, y: snake[0].y + 1 }
+    } else if (direction === "down") {
+        head = { x: snake[0].x + 1, y: snake[0].y }
+    } else if (direction === "up") {
+        head = { x: snake[0].x - 1, y: snake[0].y }
+    }
+
+    snake.forEach(segment => {
+        blocks[`${segment.x}-${segment.y}`].classList.remove("fill");
+    })
+
+    snake.unshift(head)
+    snake.pop()
+
+    render()
+}, 400);
+
 
 // console.log(board.clientHeight);
 // console.log(board.clientWidth);
