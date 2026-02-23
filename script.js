b900ce2c23e7523ef9ec374160db2f7e15849157
justforit4102 <deeppakhare19@gmail.com>
2026-02-23 16:31:22 +0530

    js game logic

--- script.js
+++ script.js
@@ -1,16 +1,35 @@
 const board = document.querySelector('.board');
-const blockHeight = 30;
-const blockWidth = 30;
+const blockHeight = 50;
+const blockWidth = 50;
 
 const cols = Math.floor(board.clientWidth / blockWidth);
 const rows = Math.floor(board.clientHeight / blockHeight);
 
-for (let i = 0; i < rows * cols; i++) {
-    const block = document.createElement('div');
-    block.classList.add("block");
-    board.appendChild(block);
+const blocks = []
+const snake = [{
+    x: 1, y: 3
+}]
+let direction = 'up'
+
+// for (let i = 0; i < rows * cols; i++) {
+//     const block = document.createElement('div');
+//     block.classList.add("block");
+//     board.appendChild(block);
+// }
+
+for (let row = 0; row < rows; row++) {
+    for (let col = 0; col < cols; col++) {
+        const block = document.createElement('div');
+        block.classList.add("block");
+        board.appendChild(block);
+        block.innerText = `${row}-${col}`
+        blocks[`${row}-${col}`] = block
+    }
 }
 
-console.log(cols);
-console.log(rows);
 
+// console.log(board.clientHeight);
+// console.log(board.clientWidth);
+// console.log(cols);
+// console.log(rows);
+// console.log(rows*cols);
